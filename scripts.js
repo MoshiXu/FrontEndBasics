af219a966e14ba2af71e4dec01b493528f3b68af
Moshi Xu <moshixu92@gmail.com>
2019-09-19 11:20:51 -0500

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,48 @@
+var a="moshi";
+a=123;
+a=true;
+a=[1,3,5];
+a=[1,"moshi",true];
+
+
+
+
+
+var student=[1,"moshi",true];
+/*
+console.log(student);
+console.log(Object.prototype.toString.call(student));
+console.log(typeof a2);
+*/
+
+
+var str='Chicago';
+/*
+console.log(str.toUpperCase());
+console.log(str.substring(0,3));
+console.log(str.indexOf('c'));
+*/
+
+var arr=[3,2,5,1];
+arr.push(2);
+//console.log(arr);
+
+arr.pop();
+
+arr.shift();
+//console.log(arr);
+arr.unshift();
+
+arr.sort(function(x,y){
+    return y=x;
+})
+console.log(arr);
+
+
+console.log(arr.slice(1,3));
+
+//undefined and notdefined
+
+//undefined: var is declared but no value is assigned
+console.log(z);
+var z='dd';
