f0d78c5ae330d3f967b88a28d2b8f89e118f5833
Moshi Xu <moshixu92@gmail.com>
2019-09-21 23:30:44 -0500

    Create test.js

---
+++ test.js
@@ -0,0 +1,18 @@
+//$--->jQuery
+
+//1
+$(".signin").on('click',()=>{
+    console.log('You clicked the button');
+})
+//2
+$(".signin").click(()=>{
+    alert('You clicked!');
+});
+
+$(".name").keyup(($event)=>{
+    console.log('You clicked the button',event);
+})
+
+
+$("<p>StarWars</p>").insertBefore(".name");
+
